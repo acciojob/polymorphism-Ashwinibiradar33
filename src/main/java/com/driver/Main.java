29de227199f109f88d88c4633137a8515a3f48a6
Ashwini Siddanagond Biradar <122633022+Ashwinibiradar33@users.noreply.github.com>
2023-07-20 20:42:05 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,41 @@
 package com.driver;
 
+//import com.sun.xml.internal.bind.v2.runtime.output.SAXOutput;
+//import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+
 public class Main {
+    public static class Product {
+        //constructor
+        //final commit
+        public int product(int x,int y)
+        {
+            return x*y;
+        }
+        public int product(int x,int y,int z)
+        {
+            return x*y*z;
+        }
+        public double product(double x,double y)
+        {
+            return x*y;
+        }
+    }
+
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(5,10);
+        p.product(5,10,5);
+        p.product(5.2,10.2);
+
+
+
+
+    }
+    //product class
+    //final commit
+
+
+
 
 }
